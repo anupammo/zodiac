9bf9ebf8e1f5a9ba49032ef677677a36cbadd25e
anupammo <a09051985@gmail.com>
2022-11-22 03:05:39 +0530

    zodiac12

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-if ("service-workers" in navigator) {
+if ("pwabuilder-sw" in navigator) {
     window.addEventListener("load", function() {
-      navigator.service-workers
-        .register("./service-workers.js")
+      navigator.pwabuilder-sw
+        .register("./pwabuilder-sw.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
